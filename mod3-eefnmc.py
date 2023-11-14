819d99eb08288534d46b68a299c3cd6af7063eaf
Erin <eefnmc@umsystem.edu>
2023-11-13 19:18:15 -0600

    added basic outline

--- mod3-eefnmc.py
+++ mod3-eefnmc.py
@@ -1,7 +1,12 @@
 #You should write unit tests for the five inputs from the stock visualizer challenge that enforce the following constraints.
 
+import unittest
+
 #?symbol: capitalized, 1-7 alpha characters
 #*?chart type: 1 numeric character, 1 or 2
 #?time series: 1 numeric character, 1 - 4
 #?start date: date type YYYY-MM-DD
 #?end date: date type YYYY-MM-DD
+
+if __name__ == '__main__':
+   unittest.main()
